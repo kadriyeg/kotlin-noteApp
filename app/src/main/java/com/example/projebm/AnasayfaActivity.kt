15064850e4587bca87d4e21adef294fedffe9b52
irem <mermer.iremnur@gmail.com>
2022-09-04 10:17:22 +0300

    ilk commit

---
+++ app/src/main/java/com/example/projebm/AnasayfaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projebm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnasayfaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anasayfa)
+    }
+}
--- app/src/main/java/com/example/projebm/MainActivity.kt
+++ app/src/main/java/com/example/projebm/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.projebm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+    }
+
+    fun girisYap(view: View){
+
+    }
+
+    fun KayitOl(view: View){
+
+        val email = emailText.text.toString()
+        val sifre = passwordText.text.toString()
+
+        auth.createUserWithEmailAndPassword(email,sifre).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                val intent = Intent(this,AnasayfaActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show()
+        }
+
     }
 }
